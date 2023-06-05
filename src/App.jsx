ff11be5ac2885e3e77175d1b7756536e4a9911bf
Lutiero <lutiero@gmail.com>
2023-06-05 14:51:30 -0400

    Criado o navbar com as rotas

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
 import React from 'react';
+import './App.css';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Header from "./Components/Header.jsx";
+import Footer from "./Components/Footer.jsx";
+import Home from "./Components/Home.jsx";
+import Login from "./Components/Login/Login.jsx";
 
 const App = () => {
-    return <div>App React</div>;
+    return (
+        <div>
+            <BrowserRouter>
+            <Header/>
+                <Routes>
+                    <Route path={'/'} element={<Home />} />
+                    <Route path={'login'} element={<Login />}/>
+                </Routes>
+            <Footer/>
+            </BrowserRouter>
+        </div>
+    );
 };
 
 export default App;
