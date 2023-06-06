a7af63de6e30902c5a788b3b2f98fca19d78ce66
Lutiero <lutiero@gmail.com>
2023-06-06 11:00:42 -0400

    USER_GET - pegando o usuario se estiver com token no localstorage

--- src/Components/Login/LoginForm.jsx
+++ src/Components/Login/LoginForm.jsx
@@ -5,29 +5,43 @@ import Input from "../Forms/Input.jsx";
 import Button from "../Forms/Button.jsx";
 import styles from "../Forms/Input.module.css";
 import useForm from "../../Hooks/useForm.jsx";
+import {TOKEN_POST, USER_GET} from "../../api.jsx";
+import login from "./Login.jsx";
 
 const LoginForm = () => {
     const username = useForm();
     const password = useForm();
-    const [token, setToken] = React.useState(null);
 
-    function handleSubmit(event) {
+    React.useEffect(() => {
+        const token = localStorage.getItem('token');
+        if (token) {
+            getUser(token);
+        }
+
+    }, []);
+
+    async function getUser(token) {
+        const {url, options} = USER_GET(token);
+        const response = await fetch(url, options);
+        const json = await response.json();
+        console.log(json);
+        return json;
+    }
+
+    async function handleSubmit(event) {
         event.preventDefault();
 
         if (username.validate() && password.validate()) {
-            fetch('https://dogsapi.origamid.dev/json/jwt-auth/v1/token', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify()
-            })
-                .then(response => response.json())
-                .then(json => {
-                    setToken(json);
-                    localStorage.setItem('token', JSON.stringify(token));
-                });
+            const {url, options} = TOKEN_POST(
+                {
+                    username: username.value,
+                    password: password.value
+                }
+            );
 
+            const response = await fetch(url, options);
+            const json = await response.json();
+            localStorage.setItem('token', json.token);
         }
     }
 
@@ -35,8 +49,12 @@ const LoginForm = () => {
         <section>
             <h1>Login</h1>
             <form onSubmit={handleSubmit}>
-                <Input name="username" label="Usuário" type="text" {...username} />
-                <Input name="password" label="Senha" type="password" {...password} />
+                <Input name="username"
+                       label="Usuário"
+                       type="text" {...username} />
+                <Input name="password"
+                       label="Senha"
+                       type="password" {...password} />
                 <Button>Entrar</Button>
             </form>
             <Link to={'/login/sign-up'}>Cadastro</Link>
